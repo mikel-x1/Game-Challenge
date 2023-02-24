f875343220982ab59522cb0d772f154423281fb7
Mikhail SEMENENKO <mikel-x1@yandex.ru>
2023-02-24 15:24:38 +0100

    Add menu

--- game_window.py
+++ game_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import clr
 clr.AddReference('System.Windows.Forms')
-from System.Windows.Forms import Form, FormBorderStyle
+from System.Windows.Forms import Form, FormBorderStyle, MenuStrip, ToolStripControlHost, ToolStripMenuItem
 clr.AddReference('System.Drawing')
 from System.Drawing import Size
 
@@ -21,6 +21,24 @@ class GameWindow(Form):
         self._rows = rows
         self._columns = columns
         self.Size = self._generate_window_size(rows, columns)
+        self.ClientSize = self._generate_window_size(rows, columns)
+        self._initialize_components()
+
+    def _initialize_components(self):
+        self._generate_menu_strip()
+
+    def _generate_menu_strip(self):
+        menu_strip = MenuStrip()
+        menu_strip.Parent = self
+
+        file_item = ToolStripMenuItem("File")
+        menu_strip.Items.Add(file_item)
+
+        new_game = ToolStripMenuItem("New game")
+        file_item.DropDownItems.Add(new_game)
+
+        self._exit = ToolStripMenuItem("Exit")
+        file_item.DropDownItems.Add(self._exit)
 
     def _generate_window_size(self, rows, columns):
         width = self._indent_left + columns * self._cell_side + self._indent_right + 10
